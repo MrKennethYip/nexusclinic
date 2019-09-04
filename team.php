c688d0c98b486942be5e818bf863a4d6590a8a58
David Kong <dave.a.kong@gmail.com>
2019-09-04 08:21:44 -0400

    Added Vithursun Yugadeva as new hire

--- team.php
+++ team.php
@@ -215,6 +215,20 @@
           </div>
           <!-- End Item -->
 
+          <!-- Item -->
+          <div class="cbp-item rmt">
+            <a class="cbp-caption" href="profiles/vithursun_yugadeva">
+              <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/vithursun_yugadeva.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/vithursun_yugadeva.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/katelyn_morgan.jpg 1000w, , https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/vithursun_yugadeva.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Vithursun Yugadeva - Registered Massage Therapy">
+              <div class="py-3">
+                <h4 class="h6 text-dark mb-0">Vithursun Yugadeva</h4>
+                <p class="small mb-0">
+                  Registered Massage Therapist<br><br>
+                </p>
+              </div>
+            </a>
+          </div>
+          <!-- End Item -->
+
         </div>
         <!-- End Content -->
       </div>       
