afd5294e0e26465d241554782246618f8d6a30ef
David Kong <dave.a.kong@gmail.com>
2019-11-14 20:18:51 -0500

    Fix broken link

--- service/physiotherapy.php
+++ service/physiotherapy.php
@@ -628,7 +628,7 @@
 
           <!-- Item -->
           <div class="cbp-item physio">
-            <a class="cbp-caption" href="profiles/mai_hoang">
+            <a class="cbp-caption" href="../profiles/mai_hoang">
               <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/mai_hoang.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/mai_hoang.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/mai_hoang.jpg 1000w, , https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/mai_hoang.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Mai Hoang - Physiotherapist">
             </a>
               <div class="py-3">
