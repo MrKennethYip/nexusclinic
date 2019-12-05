2104e0819d8872229d6b15a7064de547f4fcbc2b
David Kong <dave.a.kong@gmail.com>
2019-12-04 20:19:28 -0500

    Updated index page - $30 off physio

--- index.php
+++ index.php
@@ -177,7 +177,7 @@
     <div class="text-white text-center" style="background-color:#FC4237">
       <div class="container p-3 post">
         <span class="h4 d-block d-lg-inline-block font-weight-light mb-lg-0 font-weight-bold">
-           Nexus Holiday Promotion! Purchase a $100 Nexus gift card and receive a free gift!<br><a class="text-white" href="https://nexushealthclinic.com/gift-cards" target="blank"><u>Click Here</u> to learn more.
+           $30 OFF Initial Physiotherapy Appointment until Dec 31, 2019.!<br><a class="text-white" href="https://www.instagram.com/p/B5qOzoulAOn/" target="blank"><u>Click Here</u> to learn more.
         </span>
       </div>
     </div>
