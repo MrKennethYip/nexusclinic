18504d1b44603a2b0e65707fa66c4353eb56727b
David Kong <dave.a.kong@gmail.com>
2020-01-05 13:59:09 -0500

    Fix Ulyses Hours again =)

--- profiles/ulyses_cadiz.php
+++ profiles/ulyses_cadiz.php
@@ -139,7 +139,7 @@
 
                   <div class="ml-2">
                     <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>10am - 9pm</p>
-                    <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>10am - 7pm</p>
+                    <p class="text-dark"><strong>Sun:</strong><span class="ml-3"></span>10am - 7pm</p>
                   </div>
                 </span>
                 <!-- End User -->
