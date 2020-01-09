05c6a3c7a8aa7a6ad6c31ee1450903e26f902ec7
David Kong <dave.a.kong@gmail.com>
2020-01-09 12:15:05 -0500

    Added 15 min consultation to new osteo serv

--- pricing.php
+++ pricing.php
@@ -306,9 +306,9 @@
                       <!-- Content -->
                       <div class="u-space-1-top">
                         <ul class="list-unstyled">
+                          <li class="u-pricing-v2__list-item py-1">15 Min Consultation - <strong>Free</strong></li>
                           <li class="u-pricing-v2__list-item py-1">45 Min Follow Up - <strong>$100</strong></li>
                           <li class="u-pricing-v2__list-item py-1">60 Min Follow Up - <strong>$120</strong></li>
-                          <li class="u-pricing-v2__list-item py-1">&nbsp</li>
                           <li class="u-pricing-v2__list-item py-1">&nbsp</li>
                           <li class="u-pricing-v2__list-item py-1">&nbsp</li>
                           <li class="small text-muted py-1"><strong>**Osteopathy does not qualify for <br>direct billing**</strong><br><br>Please refer to your insurance provider for coverage. Please note that we are unable to accept insurance coverage from <u>Great West Life</u> and <u>Group Health</u> at this time.</li>
