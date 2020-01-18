7543749e0b9d23b6024d404ffd289e1d5d120679
David Kong <dave.a.kong@gmail.com>
2020-01-18 15:15:40 -0500

    Updated Yadi’s hours

--- profiles/yathaven_gopalasingam.php
+++ profiles/yathaven_gopalasingam.php
@@ -139,6 +139,7 @@
 
                   <div class="ml-2">
                     <p class="text-dark"><strong>Wed:</strong><span class="ml-3"></span>10am - 2:30pm</p>
+                    <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>1pm - 7pm</p>
                     <p class="text-dark"><strong>Sun:</strong><span class="ml-3"></span>10am - 7pm</p>
                   </div>
                 </span>
