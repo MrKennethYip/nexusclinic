3eef50f7cc8d3be783e3517de37f06331bd0451d
David Kong <dave.a.kong@gmail.com>
2020-02-24 19:12:56 -0500

    Added Kyle Ngo to website

---
+++ profiles/kyle_ngo.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Title -->
+  <title>Kyle Ngo | Registered Massage Therapist | Nexus Massage</title>
+
+  <meta name="Description" content="Meet our team! Kyle Ngo is a massage therapist at our Yonge & Eglinton clinic who specializes in Myofascial release, deep tissue & exercise prescription.">
+  <meta name="keywords" content="Kyle Ngo, Kyle, Ngo, Best RMT, deep tissue, back pain, neck pain, massage at yonge and eglinton, best massage therapist, muscle aches, sore muscles, muscle spasm, proper posture, ergonomic assessment, strong massage therapist, deep pressure, professional massage therapist, massage for back pain, massage to decrease neck pain, massage to help back pain, best massage therapist, back problems, back relief, upper back pain, what helps with back pain, sore back, chronic neck pain, back pain relief, lower back pain relief, lumbar spine, back strain, neck problems, sore neck, backache, back strain, neck and shoulder tension">
+
+  <!-- Common Header -->
+  <?php include('../includes/common-header.php') ?>
+  <!-- End Common Header -->
+
+  <style>
+    @media (min-width: 768px) {
+      .custom-top-spacing {
+        padding-top: 8.125rem;
+      }
+    }
+    @media (max-width: 1024px) {
+      .custom-top-spacing {
+        padding-top: 15rem;
+      }
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <!-- ========== HEADER ========== -->
+  <?php include('../includes/nav.php') ?>
+  <!-- ========== END HEADER ========== -->
+
+  <!-- ========== MAIN ========== -->
+  <main id="content" role="main">
+
+    <!-- Author Section -->
+    <div class="position-relative text-center custom-top-spacing u-gradient-overlay-half-info-v1 u-bg-img-hero">
+      <div class="row justify-content-md-center">
+
+        <div class="col-md-8 col-lg-7 col-xl-5">
+
+          <div class="animated bounceIn">
+          <!-- Info -->
+          <div class="mb-4">
+            <h1 class="display-4 font-size-48--md-down text-white font-weight-bold">KYLE NGO</h1>
+          </div>
+
+          <ul class="list-inline">
+            <li class="list-inline-item">
+              <a class="u-text-light" href="mailto:kyle@nexushealthclinic.com">
+                <span class="far fa-envelope mr-2"></span>
+                kyle@nexushealthclinic.com
+              </a>
+            </li>
+          </ul>
+          <!-- End Info -->
+
+          <!-- Avatar -->
+          <img class="img-fluid u-xl-avatar u-xl-avatar--bordered u-avatar-v1 rounded-circle mx-auto" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/kyle_ngo.jpg" alt="Kyle Ngo - Registered Massage Therapist">
+          <!-- End Avatar -->
+          </div>
+        </div>
+
+        <!-- SVG Background -->
+        <figure class="position-absolute-bottom-0 z-index-minus-1">
+          <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="100%" height="200px" viewBox="20 -20 300 100" style="margin-bottom: -8px;" xml:space="preserve">
+            <path class="u-fill-white" opacity="0.4" d="M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729
+              c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z" />
+            <path class="u-fill-white" opacity="0.4" d="M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729
+              c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z" />
+            <path class="u-fill-white" opacity="0" d="M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716
+              H42.401L43.415,98.342z" />
+            <path class="u-fill-white" d="M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428
+              c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z" />
+          </svg>
+        </figure>
+        <!-- End SVG Background Section -->
+      </div>
+
+    </div>
+    <!-- End Author Section -->  
+
+    <!-- Content Section -->
+    <div class="u-bg-light-blue-50">
+      <div class="container u-space-2">
+        <div class="row">
+          <div class="col-lg-3 mb-7 mb-lg-0">
+            <!-- Profile Card -->
+            <div class="card p-1 mb-4" style="min-width: 225px;">
+              <div class="card-body text-center">
+                <div class="mb-2">
+
+                  <a class="btn btn-block btn-danger transition-3d-hover" href="https://nexusclinic.clinicsense.com/book/" target="_blank" onclick="handleClick('sidebarBtn');">Book Practitioner<span class="fa fa-angle-right ml-2"></span></a>
+                  <br>
+
+                  <a class="btn btn-block u-btn-primary--air transition-3d-hover" href="mailto:kyle@nexushealthclinic.com">
+                    <span class="far fa-envelope mr-2"></span>
+                    Send a Message
+                  </a>
+                </div>
+              </div>
+            </div>
+            <!-- End Profile Card -->
+
+            <!-- Contacts  -->
+            <div class="card p-1 mb-4" style="min-width: 225px;">
+              <div class="card-body">
+                <h4 class="h6 mb-0 font-weight-bold">SERVICES:</h4>
+
+                <hr>
+
+                <!-- User -->
+                <div class="card-header border-0 py-3 d-flex align-items-center">
+                  <img src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/icons/massage-icon.jpg" class="rounded-circle u-avatar align-self-start mr-3">
+                  <div>
+                    <a href="../service/massage">
+                      <span class="d-block text-dark small font-weight-bold">Registered Massage Therapy</span>
+                    </a>
+                  </div>
+                </div>
+                <!-- End User -->
+                
+              </div>
+            </div>
+            <!-- End Contacts  -->
+
+            <!-- Hours  -->
+            <div class="card p-1 mb-4" style="min-width: 225px;">
+              <div class="card-body">
+                <h4 class="h6 mb-0 font-weight-bold">HOURS OF AVAILABILTY:</h4>
+
+                <hr>
+
+                <!-- User -->
+                <span class="d-flex align-items-start">
+
+                  <div class="ml-2">
+                    <p class="text-dark"><strong>Tues:</strong><span class="ml-3"></span>1pm - 9pm</p>
+                    <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>10am - 9pm</p>
+                    <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>1pm - 7pm</p>
+                  </div>
+                </span>
+                <!-- End User -->
+                  
+              </div>
+            </div>
+            <!-- End Hours  -->
+
+          </div>
+
+          <div class="col-lg-9">
+              <!-- Title -->
+              <div class="mb-3">
+                <h5 class="text-primary font-weight-bold">Tell us about yourself?</h5>
+              </div>
+              <!-- End Title -->
+
+              <!-- Text -->
+              <p>I am a Registered Massage Therapist from Sutherland-Chan School of Massage Therapy. I have always been interested in health care which led me to pursue my Bachelor of Arts in Kinesiology and Health Sciences from York University. Through being involved in various sports and learning about health and fitness and athletic injuries, I chose Massage Therapy as a career to better educate myself in helping others.</p>
+
+              <p>Outside the clinic, I compete with my dragon boat team as well as coach my former university team. You can also find me working out at the gym, spending time with family and friends, and occasionally catching a Raptors or Leafs game.</p> 
+              <!-- End Text -->
+
+              <!-- Title -->
+              <div class="mb-3">
+                <h5 class="text-primary font-weight-bold">Which cases do you enjoy treating? Why?</h5>
+              </div>
+              <!-- End Title -->
+
+              <!-- Text -->
+              <p>Every case is a learning experience for me no matter if you are a regular athlete, weekend warrior, or someone who just needs maintenance for everyday tasks. I make sure I approach each case by targeting the root cause of the pain or limitation and educating my clients to ensure they are fully informed and have the tools necessary when leaving the clinic.</p>
+              <!-- End Text -->
+
+              <!-- Title -->
+              <div class="mb-3">
+                <h5 class="text-primary font-weight-bold">What are the top 3 treatments that you specialize in?</h5>
+              </div>
+              <!-- End Title -->
+
+              <!-- Text -->
+              <p>I like to use myofascial release as well as trigger point therapy in my treatments. I find these methods helpful in reducing muscle resting tension, pain, and improving range of motion.</p>
+              <p>I also like incorporating dynamic release techniques in my treatments. Dynamic release uses active and passive movements to treat different muscles. These techniques can also be similarly replicated into self-care routines to ensure wellness outside of the clinic and prolong the effectiveness of each treatment.</p>
+              <!-- End Text -->
+
+              <!-- Title -->
+              <div class="mb-3">
+                <h5 class="text-primary font-weight-bold">What are you proud of in your practice?</h5>
+              </div>
+              <!-- End Title -->
+
+              <!-- Text -->
+              <p>My goal with every client is to help them better understand their bodies and provide them the tools necessary to achieve their treatment goals. Client education is an important part of my practice and I feel proud when a client tells me that my work has made a difference in their life.</p>
+              <!-- End Text -->
+
+              <!-- Title -->
+              <div class="mb-3">
+                <h5 class="text-primary font-weight-bold">Why should patients choose you as their provider instead of others in the area?</h5>
+              </div>
+              <!-- End Title -->
+
+              <!-- Text -->
+              <p>I am always driven to finding the answer to any problem at hand. I make sure I listen to every detail and draw from my education and experience in healthcare to deliver an effective treatment plan for recovery and provide the resources for a healthy pain-free lifestyle.</p>
+              <!-- End Text -->
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    <!-- End Content Section -->
+
+    <!-- Social Section -->
+    <div class="u-bg-light-blue-50 u-space-2-bottom">
+      <div class="container">
+        <!-- Review Widget -->
+        <div class='embedsocial-reviews' data-ref="f8d52c301e11304b7836b9e05d1add91f3158957"></div>
+
+        <script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/ri.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialReviewsScript"));</script>
+        <!-- End Review Widget -->
+      </div>
+    </div>
+
+  </main>
+  <!-- ========== END MAIN ========== -->
+
+  <!-- ========== FOOTER ========== -->
+  <?php include('../includes/footer.php')?>
+  <!-- ========== END FOOTER ========== -->
+
+  <!-- Common Footer -->
+  <?php include('../includes/common-footer.php') ?>
+  <!-- End Common Footer -->
+
+</body>
+</html>
--- team.php
+++ team.php
@@ -365,6 +365,22 @@
           <!-- End Item -->
 
           <!-- Item -->
+          <div class="cbp-item rmt">
+            <a class="cbp-caption" href="profiles/kyle_ngo">
+              <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/kyle_ngo.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/kyle_ngo.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/kyle_ngo.jpg 1000w, , https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/kyle_ngo.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Kyle Ngo - Registered Massage Therapy">
+            </a>
+              <div class="py-3">
+                <a href="profiles/kyle_ngo">
+                  <h4 class="h6 text-dark mb-0">Kyle Ngo</h4>
+                </a>
+                <p class="small mb-0">
+                  Registered Massage Therapist<br><br>
+                </p>
+              </div>
+          </div>
+          <!-- End Item -->
+
+          <!-- Item -->
           <div class="cbp-item">
             <a class="cbp-caption" href="careers">
               <img class="rounded p-2 responsively-lazy" style="border: #00C3DC 3px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/new_hire.png" alt="We are Hiring">
