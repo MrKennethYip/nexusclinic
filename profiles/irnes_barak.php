f7df8d861db40500e9f485799f4eca2529ed9170
David Kong <dave.a.kong@gmail.com>
2020-06-24 08:32:00 -0400

    Irnes Hours

--- profiles/irnes_barak.php
+++ profiles/irnes_barak.php
@@ -145,10 +145,8 @@
                 <span class="d-flex align-items-start">
 
                   <div class="ml-2">
-                    <p class="text-dark"><strong>Wed:</strong><span class="ml-3"></span>2pm - 9pm</p>
-                    <p class="text-dark"><strong>Wed:</strong><span class="ml-3"></span>2:30pm - 9pm</p>
-                    <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>10am - 9pm</p>
-                    <p class="text-dark"><strong>Sat - Sun:</strong><span class="ml-3"></span>2pm - 7pm</p>
+                    <p class="text-dark"><strong>Mon:</strong><span class="ml-3"></span>10am - 9pm</p>
+                    <p class="text-dark"><strong>Sat - Sun:</strong><span class="ml-3"></span>10am - 9pm</p>
                     
                   </div>
                 </span>
