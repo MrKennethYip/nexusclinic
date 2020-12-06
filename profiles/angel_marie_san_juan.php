6ea7ea65a3ff23a97212d4eff9b9bfdad7e99578
David Kong <dave.a.kong@gmail.com>
2020-12-06 15:55:21 +0000

    Updates to hours

--- profiles/angel_marie_san_juan.php
+++ profiles/angel_marie_san_juan.php
@@ -140,8 +140,8 @@
                 <span class="d-flex align-items-start">
 
                   <div class="ml-2">
-                    <p class="text-dark"><strong>Mon-Wed:</strong><span class="ml-3"></span>10am - 9pm</p>
-                    <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>10am - 7pm</p>
+                    <p class="text-dark"><strong>Mon-Thurs:</strong><span class="ml-3"></span>11am - 8pm</p>
+                    <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>11am - 7pm</p>
                   </div>
                 </span>
                 <!-- End User -->
