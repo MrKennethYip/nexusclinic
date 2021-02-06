b6e14ce413b7dabbe6f432736766b9b73db095c0
David Kong <dave.a.kong@gmail.com>
2021-02-06 17:32:59 +0000

    Update matt’s hours - remove cheryl

--- profiles/matt_walton.php
+++ profiles/matt_walton.php
@@ -154,9 +154,8 @@
                 <span class="d-flex align-items-start">
 
                   <div class="ml-2">
-                    <p class="text-dark"><strong>Tues:</strong><span class="ml-3"></span>2pm - 9pm</p>
+                    <p class="text-dark"><strong>Tues:</strong><span class="ml-3"></span>10am - 9pm</p>
                     <p class="text-dark"><strong>Wed:</strong><span class="ml-3"></span>2pm - 9pm</p>
-                    <p class="text-dark"><strong>Thurs:</strong><span class="ml-3"></span>2pm - 9pm</p>
                     <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>10am - 9pm</p>
                   </div>
                 </span>
