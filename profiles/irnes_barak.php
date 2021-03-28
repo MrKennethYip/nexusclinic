f19f2194df5253e29c5561a69d22bc8badc1d68e
David Kong <dave.a.kong@gmail.com>
2021-03-28 13:25:12 +0000

    Update to Irnes hours

--- profiles/irnes_barak.php
+++ profiles/irnes_barak.php
@@ -145,8 +145,8 @@
                 <span class="d-flex align-items-start">
 
                   <div class="ml-2">
-                    <p class="text-dark"><strong>Mon:</strong><span class="ml-3"></span>2pm - 7pm</p>
-                    <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>10am - 7pm</p>
+                    <p class="text-dark"><strong>Mon:</strong><span class="ml-3"></span>10am - 8pm</p>
+                    <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>10am - 8pm</p>
                     <p class="text-dark"><strong>Sun:</strong><span class="ml-3"></span>1pm - 7pm</p>
                     
                   </div>
