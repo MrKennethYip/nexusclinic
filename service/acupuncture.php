089ba0de441149d7d3514ce7fccd16e1b47521f2
David Kong <dave.a.kong@gmail.com>
2021-04-22 03:33:12 +0000

    Remove Ellen from acupuncture page

--- service/acupuncture.php
+++ service/acupuncture.php
@@ -471,22 +471,6 @@
           </div>
           <!-- End Item -->
 
-          <!-- Item -->
-          <div class="cbp-item rmt">
-            <a class="cbp-caption" href="../profiles/ellen_zhang">
-              <img class="rounded p-2" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/ellen_zhang.jpg" srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/ellen_zhang.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/ellen_zhang.jpg 1000w, , https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/ellen_zhang.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Ellen Zhang - Acupuncture">
-            </a>
-              <div class="py-3">
-                <a href="../profiles/ellen_zhang">
-                  <h4 class="h6 text-dark mb-0">Ellen Zhang</h4>
-                </a>
-                <p class="small mb-0">
-                  I have an extensive understanding of the body and its function with my education background in Massage, Acupuncture and Osteopath . I believe that with this knowledge, I am better at understanding the symptoms and problems that my patients come in with and deliver the most effective treatment to meet their goals.<br><br>
-                </p>
-              </div>
-          </div>
-          <!-- End Item -->
-
         </div>
         <!-- End Content -->
       </div>       
