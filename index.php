aece28af1085ab5075709d7a1dc22833f79114bf
Kenneth Yip <kksyip@gmail.com>
2021-06-02 02:15:20 -0400

    defer large images to load to load in background

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
           </div>
 
         </div>
-        <div class="js-slide divimage u-space-3-top height-is-based-on-content" style="background-image: url(https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/hero/lrg/home-physio.jpg);">
+        <div class="js-slide divimage u-space-3-top height-is-based-on-content" data-background-image-url="https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/hero/lrg/home-physio.jpg">
 
           <div class="animated bounceIn">
           <!-- Content -->
@@ -129,7 +129,7 @@
 
         </div>
 
-        <div class="js-slide divimage u-space-3-top height-is-based-on-content" style="background-image: url(https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/hero/lrg/home-acupuncture.jpg);">
+        <div class="js-slide divimage u-space-3-top height-is-based-on-content" data-background-image-url="https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/hero/lrg/home-acupuncture.jpg">
 
           <div class="animated bounceIn">
           <!-- Content -->
@@ -149,7 +149,7 @@
 
         </div>
 
-        <div class="js-slide divimage u-space-3-top height-is-based-on-content" style="background-image: url(https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/hero/lrg/home-orthotics.jpg);">
+        <div class="js-slide divimage u-space-3-top height-is-based-on-content" data-background-image-url="https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/hero/lrg/home-orthotics.jpg">
 
           <div class="animated bounceIn">
           <!-- Content -->
@@ -1017,6 +1017,70 @@
   });
   </script>
   -->
+
+  <script>
+  function BackgroundNode({node, loadedClassName}) {
+  	let src = node.getAttribute('data-background-image-url');
+  	let show = (onComplete) => {
+  		requestAnimationFrame(() => {
+  			node.style.backgroundImage = `url(${src})`
+  			node.classList.add(loadedClassName);
+  			onComplete();
+  		})
+  	}
+
+  	return {
+  		node,
+
+  		// onComplete is called after the image is done loading.
+  		load: (onComplete) => {
+  			let img = new Image();
+  			img.onload = show(onComplete);
+  			img.src = src;
+  		}
+  	}
+  }
+
+  let defaultOptions = {
+  	selector: '[data-background-image-url]',
+  	loadedClassName: 'loaded'
+  }
+
+  function BackgroundLazyLoader({selector, loadedClassName} = defaultOptions) {
+  	let nodes = [].slice.apply(document.querySelectorAll(selector))
+  		.map(node => new BackgroundNode({node, loadedClassName}));
+
+  	let callback = (entries, observer) => {
+  		entries.forEach(({target, isIntersecting}) => {
+  			if (!isIntersecting) {
+  				return;
+  			}
+
+  			let obj = nodes.find(it => it.node.isSameNode(target));
+
+  			if (obj) {
+  				obj.load(() => {
+  					// Unobserve the node:
+  					observer.unobserve(target);
+  					// Remove this node from our list:
+  					nodes = nodes.filter(n => !n.node.isSameNode(target));
+
+  					// If there are no remaining unloaded nodes,
+  					// disconnect the observer since we don't need it anymore.
+  					if (!nodes.length) {
+  						observer.disconnect();
+  					}
+  				});
+  			}
+  		})
+  	};
+
+  	let observer = new IntersectionObserver(callback);
+  	nodes.forEach(node => observer.observe(node.node));
+  };
+
+  BackgroundLazyLoader();
+  </script>
 
   <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtt1z99GtrHZt_IcnK-wryNsQ30A112J0&callback">
