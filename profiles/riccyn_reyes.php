522e62b41e69462c95c8f857a4a09b7c7f3e925d
David Kong <dave.a.kong@gmail.com>
2021-06-08 10:46:15 +0000

    Update hours for Riccyn

--- profiles/riccyn_reyes.php
+++ profiles/riccyn_reyes.php
@@ -140,10 +140,10 @@
                 <span class="d-flex align-items-start">
 
                   <div class="ml-2">
-                    <p class="text-dark"><strong>Mon:</strong><span class="ml-3"></span>11am - 8pm</p>
-                    <p class="text-dark"><strong>Tues:</strong><span class="ml-3"></span>11am - 8pm</p>
-                    <p class="text-dark"><strong>Wed:</strong><span class="ml-3"></span>11am - 8pm</p>
-                    <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>11am - 7pm</p>
+                    <p class="text-dark"><strong>Tues:</strong><span class="ml-3"></span>10am - 9pm</p>
+                    <p class="text-dark"><strong>Wed:</strong><span class="ml-3"></span>10am - 9pm</p>
+                    <p class="text-dark"><strong>Thurs:</strong><span class="ml-3"></span>10am - 9pm</p>
+                    <p class="text-dark"><strong>Sun:</strong><span class="ml-3"></span>10am - 7pm</p>
                   </div>
                 </span>
                 <!-- End User -->
