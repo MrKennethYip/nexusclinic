0da7b4816e811289d27d0a141dcb2242751377ed
David Kong <dave.a.kong@gmail.com>
2021-06-08 02:09:51 +0000

    Added Riccyn to website

--- team.php
+++ team.php
@@ -359,6 +359,22 @@
           <!-- End Item -->
             
           <!-- Item -->
+          <div class="cbp-item rmt">
+            <a class="cbp-caption" href="profiles/riccyn_reyes">
+              <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/riccyn_reyes.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/riccyn_reyes.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/riccyn_reyes.jpg 1000w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/riccyn_reyes.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Riccyn Reyes - Registered Massage Therapy">
+            </a>
+              <div class="py-3">
+                <a href="profiles/riccyn_reyes">
+                  <h4 class="h6 text-dark mb-0">Riccyn Reyes</h4>
+                </a>
+                <p class="small mb-0">
+                  Registered Massage Therapist<br><br>
+                </p>
+              </div>
+          </div>
+          <!-- End Item -->
+            
+          <!-- Item -->
           <div class="cbp-item physio">
             <a class="cbp-caption" href="profiles/victoria_kwong">
               <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/victoria_kwong.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/victoria_kwong.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/victoria_kwong.jpg 1000w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/victoria_kwong.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Victoria Kwong - Physiotherapist">
