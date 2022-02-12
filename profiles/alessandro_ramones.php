2ade181b557ba1fe584870ba481f7baad054388f
David Kong <dave.a.kong@gmail.com>
2022-02-12 02:53:22 +0000

    Alessandro updates

--- profiles/alessandro_ramones.php
+++ profiles/alessandro_ramones.php
@@ -140,7 +140,6 @@
                 <span class="d-flex align-items-start">
 
                   <div class="ml-2">
-                    <p class="text-dark"><strong>Thurs:</strong><span class="ml-3"></span>10am - 9pm</p>
                     <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>10am - 9pm</p>
                     <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>10am - 7pm</p>
                     <p class="text-dark"><strong>Sun:</strong><span class="ml-3"></span>10am - 7pm</p>
