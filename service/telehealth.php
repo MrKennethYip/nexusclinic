78fe4dffb58d2c78cab7c7e34f3c2efb51808aaa
David Kong <dave.a.kong@gmail.com>
2023-01-01 02:27:00 +0000

    remove Taral from staff selection - telehealth

--- service/telehealth.php
+++ service/telehealth.php
@@ -400,22 +400,6 @@
             
           <!-- Item -->
           <div class="cbp-item physio">
-            <a class="cbp-caption" href="../profiles/taral_shah">
-              <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/profile/med/taral_shah.jpg" data-srcset="https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/profile/med/taral_shah.jpg 500w, https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/profile/med/taral_shah.jpg 1000w, https://nexusmassageclinic.s3.ca-central-1.amazonaws.com/images/profile/lrg/taral_shah.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Taral Shah - Physiotherapy Resident">
-            </a>
-              <div class="py-3">
-                <a href="../profiles/taral_shah">
-                  <h4 class="h6 text-dark mb-0">Taral Shah</h4>
-                </a>
-                <p class="small mb-0">
-                  Physiotherapy Resident<br><br>
-                </p>
-              </div>
-          </div>
-          <!-- End Item -->
-            
-          <!-- Item -->
-          <div class="cbp-item physio">
             <a class="cbp-caption" href="../profiles/victoria_kwong">
               <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/victoria_kwong.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/victoria_kwong.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/victoria_kwong.jpg 1000w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/victoria_kwong.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Victoria Kwong - Physiotherapist">
             </a>
