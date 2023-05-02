38ec811aa865629f99cebccf64b7424405ea12ed
David Kong <dave.a.kong@gmail.com>
2023-05-02 12:44:36 +0000

    Remoev David Tam from website

--- team.php
+++ team.php
@@ -261,22 +261,6 @@
               </div>
           </div>
           <!-- End Item -->
-              
-          <!-- Item -->
-          <div class="cbp-item chiro">
-            <a class="cbp-caption" href="profiles/david_tam">
-              <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/david_tam.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/david_tam.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/david_tam.jpg 1000w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/david_tam.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Dr. David Tam - Chiropractor">
-            </a>
-              <div class="py-3">
-                <a href="profiles/david_tam">
-                  <h4 class="h6 text-dark mb-0">Dr. David Tam</h4>
-                </a>
-                <p class="small mb-0">
-                  Chiropractor, Registered Massage Therapist, Acupuncture Provider
-                </p>
-              </div>
-          </div>
-          <!-- End Item -->
             
           <!-- Item -->
           <div class="cbp-item rmt">
