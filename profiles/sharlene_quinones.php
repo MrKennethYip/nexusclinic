8d1379b5322bde1b01afa5008d3261dc6a85c5f1
MrKennethYip <kksyip@gmail.com>
2023-10-22 14:25:56 -0400

    Update sharlene_quinones.php
    update shar's hours

--- profiles/sharlene_quinones.php
+++ profiles/sharlene_quinones.php
@@ -141,6 +141,7 @@
 
                   <div class="ml-2">
                     <p class="text-dark"><strong>Mon:</strong><span class="ml-3"></span>8am - 2pm</p>
+                    <p class="text-dark"><strong>Tues:</strong><span class="ml-3"></span>8am - 2pm</p>
                     <p class="text-dark"><strong>Wed:</strong><span class="ml-3"></span>8am - 2pm</p>
                     <p class="text-dark"><strong>Fri:</strong><span class="ml-3"></span>8am - 2pm</p>
                     <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>8am - 7pm</p>
