92993dd592e830408cb7d1ab78a5531c68852659
MrKennethYip <kksyip@gmail.com>
2023-11-24 13:07:58 -0500

    update aimees hours

--- profiles/aimee_leung.php
+++ profiles/aimee_leung.php
@@ -137,7 +137,7 @@
 
                   <div class="ml-2">
                     <p class="text-dark"><strong>Mon:</strong><span class="ml-3"></span>2pm - 9pm</p>
-                    <p class="text-dark"><strong>Sat:</strong><span class="ml-3"></span>2pm - 9pm</p>
+                    <p class="text-dark"><strong>Thurs:</strong><span class="ml-3"></span>2pm - 9pm</p>
                     <p class="text-dark"><strong>Sun:</strong><span class="ml-3"></span>2pm - 7pm</p>
                   </div>
                 </span>
