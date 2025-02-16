d729bcf602f82bd7843a038977bb2299d25fe238
MrKennethYip <kksyip@gmail.com>
2025-02-16 01:47:17 -0500

    Adding richard

--- team.php
+++ team.php
@@ -631,6 +631,18 @@
           </div>
           <!-- End Item -->
 
+          <!-- Item -->
+          <div class="cbp-item rmt chiro acupuncture physio osteopath">
+            <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/richard_weisdorf.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/richard_weisdorf.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/richard_weisdorf.jpg 1000w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/richard_weisdorf.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px"  alt="Richard Weisdorf - Nexus Massage and Rehab - Toronto">
+            <div class="py-3 blurb">
+              <h4 class="h6 text-dark mb-0">Richard Weisdorf</h4>
+              <p class="small mb-0">
+                Clinic Administrator<br><br>
+              </p>
+            </div>
+          </div>
+          <!-- End Item -->
+
         </div>
         <!-- End Content -->
       </div>       
