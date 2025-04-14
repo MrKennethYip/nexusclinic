22bda8d9c0a62a94b46144c32c5b0ae5393a9a7f
MrKennethYip <kksyip@gmail.com>
2025-04-14 19:52:07 -0400

    Updating Peter and Kenneth with acupuncture

--- team.php
+++ team.php
@@ -370,7 +370,7 @@
                  <h4 class="h6 text-dark mb-0">Peter Gallis</h4>
                </a>
                <p class="small mb-0">
-                 Registered Massage Therapist<br><br>
+                 Registered Massage Therapist, Acupuncture Provider<br><br>
                </p>
              </div>
          </div>
