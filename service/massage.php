1ffc3866bf1f2c93322cef95346c68f255070096
MrKennethYip <kksyip@gmail.com>
2025-09-27 23:20:40 -0400

    remove imelda from massage

--- service/massage.php
+++ service/massage.php
@@ -1322,22 +1322,6 @@
           <!-- End Item -->
 
           <!-- Item -->
-          <div class="cbp-item rmt osteopath">
-            <a class="cbp-caption" href="../profiles/imelda_garce.php">
-              <img class="rounded p-2 responsively-lazy" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusclinicassets/images/profile/med/imelda_garce.jpg" data-srcset="https://s3.ca-central-1.amazonaws.com/nexusclinicassets/images/profile/med/imelda_garce.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusclinicassets/images/profile/med/imelda_garce.jpg 1000w, https://s3.ca-central-1.amazonaws.com/nexusclinicassets/images/profile/lrg/imelda_garce.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Imelda Garce - Registered Massage Therapist, Registered Osteopath">
-            </a>
-              <div class="py-3 blurb">
-                <a href="../profiles/imelda_garce.php">
-                  <h4 class="h6 text-dark mb-0">Imelda Garce</h4>
-                </a>
-                <p class="small mb-0">
-                  Registered Massage Therapist, Registered Osteopath<br><br>
-                </p>
-              </div>
-          </div>
-          <!-- End Item -->
-
-          <!-- Item -->
           <div class="cbp-item rmt">
             <a class="cbp-caption" href="../profiles/cassandra_kong">
               <img class="rounded p-2" style="border: #898A8D 2px solid;" src="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/cassandra_kong.jpg" srcset="https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/cassandra_kong.jpg 500w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/med/cassandra_kong.jpg 1000w, https://s3.ca-central-1.amazonaws.com/nexusmassageclinic/images/profile/lrg/cassandra_kong.jpg 1500w" sizes="(min-width: 993px) 1500px,(min-width: 768px) and (max-width: 992px) 1000px, (max-width: 767px) 500px" alt="Cassandra Kong - Registered Massage Therapy">
