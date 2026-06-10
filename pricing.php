7b5d7d41a7dd520640e7a9a3901fd1774a4b341d
Wilton Ngo <wiltonngo@gmail.com>
2026-06-09 23:27:32 -0400

    Add initial assessment to weight loss body sculpting

--- pricing.php
+++ pricing.php
@@ -511,9 +511,9 @@
                       <!-- Content -->
                       <div class="u-space-1-top">
                         <ul class="list-unstyled">
+                          <li class="u-pricing-v2__list-item py-1">15 Min Consultation - <strong>FREE</strong></li>
                           <li class="u-pricing-v2__list-item py-1">Initial Assessment - <strong>$180</strong></li>
                           <li class="u-pricing-v2__list-item py-1">60 Min Follow-Up - <strong>$160</strong></li>
-                          <li class="u-pricing-v2__list-item py-1">&nbsp</li>
                           <li class="u-pricing-v2__list-item py-1">&nbsp</li>
                           <li class="u-pricing-v2__list-item py-1">&nbsp</li>
                           <li class="u-pricing-v2__list-item py-1">&nbsp</li>
